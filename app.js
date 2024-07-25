d64bd5347857a8a29ced776f568b466d842e8005
deekshashetty013 <75476311+deekshashetty013@users.noreply.github.com>
2024-07-25 23:48:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const parkingRoutes = require('./routes/ParkingRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/smart_parking_lot', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB', err);
+});
+
+app.use('/parking', parkingRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
